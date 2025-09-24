366b65e54aa4e2c34597cde473d911c006a8f8cb
rubenaryo <rubeny484@gmail.com>
2025-09-23 23:48:16 -0400

    First pass at a diffuse bsdf

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -280,6 +280,114 @@ __global__ void shadeFakeMaterial(
     }
 }
 
+// TODO move all of these to a new file
+__device__ glm::vec3 squareToDiskConcentric(glm::vec2 xi) {
+
+    glm::vec2 corrected = 2.0f * xi - glm::vec2(1.0);
+    float x = corrected.x;
+    float y = corrected.y;
+
+    // Div by zero guard
+    if (abs(x) < 0.01 && abs(y) < 0.01)
+        return glm::vec3(0);
+
+    float r, theta;
+    if (abs(x) > abs(y))
+    {
+        r = x;
+        theta = (PI / 4.0) * (y / x);
+    }
+    else
+    {
+        r = y;
+        theta = (PI / 2.0) - (PI / 4.0) * (x / y);
+    }
+
+    return r * glm::vec3(cos(theta), sin(theta), 0.0);
+}
+
+
+inline __device__ glm::vec3 squareToHemisphereCosine(const glm::vec2& xi)
+{
+    glm::vec3 diskCoord = squareToDiskConcentric(xi);
+    diskCoord.z = glm::sqrt(glm::max(0.0f, (1.0f - diskCoord.x * diskCoord.x - diskCoord.y * diskCoord.y)));
+    return diskCoord;
+}
+
+inline __device__ Ray SpawnRay(const glm::vec3& pos, const glm::vec3& wi)
+{
+    Ray r;
+    r.origin = pos + wi * 0.001f;
+    r.direction = wi;
+    return r;
+}
+
+inline __device__ float squareToHemisphereCosinePDF(const glm::vec3& sample) {
+
+    return sample.z * (1.0 / PI);
+}
+
+__global__ void shadeMaterial(
+    int iter,
+    int num_paths,
+    ShadeableIntersection* shadeableIntersections,
+    PathSegment* pathSegments,
+    Material* materials)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < num_paths)
+    {
+        ShadeableIntersection intersection = shadeableIntersections[idx];
+        if (intersection.t > 0.0f) // if the intersection exists...
+        {
+            // Set up the RNG
+            // LOOK: this is how you use thrust's RNG! Please look at
+            // makeSeededRandomEngine as well.
+            thrust::default_random_engine rng = makeSeededRandomEngine(iter, idx, 0);
+            thrust::uniform_real_distribution<float> u01(0, 1);
+
+            Material material = materials[intersection.materialId];
+            glm::vec3 materialColor = material.color;
+
+            // If the material indicates that the object was a light, "light" the ray
+            if (material.emittance > 0.0f) {
+                pathSegments[idx].color *= (materialColor * material.emittance);
+            }
+            // Otherwise, do some pseudo-lighting computation. This is actually more
+            // like what you would expect from shading in a rasterizer like OpenGL.
+            // TODO: replace this! you should be able to start with basically a one-liner
+            else {
+                // Basically Sample_f() from 561
+                //float lightTerm = glm::dot(intersection.surfaceNormal, glm::vec3(0.0f, 1.0f, 0.0f));
+                //pathSegments[idx].color *= (materialColor * lightTerm) * 0.3f + ((1.0f - intersection.t * 0.02f) * materialColor) * 0.7f;
+                //pathSegments[idx].color *= u01(rng); // apply some noise because why not
+
+                PathSegment path = pathSegments[idx];
+                Ray wo = path.ray;
+
+                thrust::default_random_engine rng0 = makeSeededRandomEngine(iter, idx, 0);
+                thrust::default_random_engine rng1 = makeSeededRandomEngine(iter, idx, 1);
+                glm::vec2 xi = glm::vec2(u01(rng0), u01(rng1));
+
+                glm::vec3 hemisphereCosWi = squareToHemisphereCosine(xi);
+                float pdf = squareToHemisphereCosinePDF(hemisphereCosWi);
+                glm::vec3 wiW = hemisphereCosWi; // TODO: convert to world space?
+                glm::vec3 bsdf = materialColor / PI;
+
+                pathSegments[idx].color *= (bsdf * glm::abs(glm::dot(wiW, intersection.surfaceNormal))) / pdf;
+                pathSegments[idx].ray = SpawnRay(wo.origin + intersection.t*wo.direction, wiW);
+            }
+            // If there was no intersection, color the ray black.
+            // Lots of renderers use 4 channel color, RGBA, where A = alpha, often
+            // used for opacity, in which case they can indicate "no opacity".
+            // This can be useful for post-processing and image compositing.
+        }
+        else {
+            pathSegments[idx].color = glm::vec3(0.0f);
+        }
+    }
+}
+
 // Add the current iteration's output to the overall image
 __global__ void finalGather(int nPaths, glm::vec3* image, PathSegment* iterationPaths)
 {
@@ -381,7 +489,7 @@ void pathtrace(uchar4* pbo, int frame, int iter)
         // TODO: compare between directly shading the path segments and shading
         // path segments that have been reshuffled to be contiguous in memory.
 
-        shadeFakeMaterial<<<numblocksPathSegmentTracing, blockSize1d>>>(
+        shadeMaterial<<<numblocksPathSegmentTracing, blockSize1d>>>(
             iter,
             num_paths,
             dev_intersections,
